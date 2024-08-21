a92994e2aa173f9f28efcbb274beb3544e376b56
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-08-21 10:40:04 -0300

    Melhorando nosso cadastro salvando e carregando

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -22,10 +22,15 @@ public class PessoaController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value="/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
+	public ModelAndView salvar(Pessoa pessoa) {
 		
 		pessoaRepository.save(pessoa);
-		return "cadastro/cadastropessoa";
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoaIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoaIt);
+		
+		return andView;
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value="/listapessoas")
